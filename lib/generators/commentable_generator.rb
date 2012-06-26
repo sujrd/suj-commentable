ccdbfa1958263601d69e918d2d5058d1666f1a02
Horacio Sanson <horacio@skillupjapan.co.jp>
2012-06-26 17:01:04 +0900

    Fix views generator.

--- lib/generators/commentable_generator.rb
+++ lib/generators/commentable_generator.rb
@@ -1,17 +1,18 @@
 module Suj
   module Commentable
     module Generators
-      class InstallGenerator < Rails::Generators::Base
-        source_root File.expand_path("../../templates", __FILE__)
-        desc "Creates suj commentable helpers and models."
+      class ViewsGenerator < Rails::Generators::Base
+        source_root File.expand_path("../../../../../app", __FILE__)
+        desc "Adds suj commentable views and stylesheets to your application."
 
         def copy_views
-          directory "views/comments", "app/views/comments"
+          directory "views/suj", "app/views"
+        end
+        
+        def copy_style
+          directory "assets/stylesheets/suj", "app/stylesheets"
         end
 
-        def copy_model
-          template "models/comment.rb", "app/models/comment.rb"
-        end
       end
     end
   end
--- lib/suj/commentable/engine.rb
+++ lib/suj/commentable/engine.rb
@@ -7,6 +7,10 @@ module Suj
   module Commentable
     class Engine < Rails::Engine
       # auto wire
+      config.generators do |g|
+        g.orm :mongoid
+      end
+      
       initializer "suj.commentable.view_helpers" do
         ActionView::Base.send :include, Suj::Commentable::ViewHelpers
       end
