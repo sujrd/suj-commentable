e698596e6e81e51b193d440bdbed2ee7d00a5936
Fernando Wong <f.wong@skillupjapan.co.jp>
2012-06-27 16:50:41 +0900

    Added :formats => [:html] to view helpers

--- lib/suj/commentable/view_helper.rb
+++ lib/suj/commentable/view_helper.rb
@@ -2,7 +2,7 @@ module Suj
   module Commentable
     module ViewHelpers
       def comments_for(commentable, author = nil, page = 1, per = 25)
-        render :partial => 'suj/commentable/commentable', :locals => { :commentable => commentable, :author => author }
+        render :partial => 'suj/commentable/commentable', :formats => [:html], :locals => { :commentable => commentable, :author => author }
       end
     end
   end
