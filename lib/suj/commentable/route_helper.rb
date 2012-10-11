ae3b071d4bba68c1653660316a2d14173257885a
Horacio Sanson <horacio@skillupjapan.co.jp>
2012-10-11 14:29:04 +0900

    Add hide, unhide and delete comment actions.
    Now it is possible to hide, unhide and delete comments.

--- lib/suj/commentable/route_helper.rb
+++ lib/suj/commentable/route_helper.rb
@@ -5,9 +5,13 @@ module Suj
         resources :comments, :only => [] do
           put 'like', :on => :member
           put 'unlike', :on => :member
+          put 'open', :on => :member
+          put 'close', :on => :member
+          put 'hide', :on => :member
+          put 'unhide', :on => :member
         end
         resources model do
-          resources :comments, :only => [:index, :create, :destroy]
+          resources :comments, :only => [:index, :create]
         end
       end
     end
