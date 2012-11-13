24dd52a32a567ab9ecd23ffbcbd9519fb280e140
Horacio Sanson <horacio@skillupjapan.co.jp>
2012-11-13 12:44:40 +0900

    Add callbacks.

--- app/model/suj/commentable/comment.rb
+++ app/model/suj/commentable/comment.rb
@@ -13,8 +13,9 @@ module Suj
       belongs_to :commentable, polymorphic: true
       validates_presence_of :text
       validates_presence_of :commentable
-      after_create :update_root
-      before_destroy :delete_descendants
+      before_create :notify_commentable_before_create
+      after_create :update_root, :notify_commentable_after_create
+      before_destroy :delete_descendants, :notify_commentable_before_destroy
       validate :validate_max_depth
       scope :by_date_asc, asc(:replied_at)
       scope :by_date_desc, desc(:replied_at)
@@ -41,6 +42,20 @@ module Suj
 
       def enabled?
         self.deleted_at.nil?
+      end
+
+      def like!(user)
+        return if ! user
+        weight = user.rate_weight || 1
+        rate_and_save weight, user
+        notify_commentable_after_like(user)
+      end
+
+      def unlike!(user)
+        return if ! user
+        weight = (user.rate_weight || 1) * -1
+        rate_and_save weight, user
+        notify_commentable_after_unlike(user)
       end
 
       # Helper method to force destroy of the client no matter if it is enabled or
@@ -76,6 +91,25 @@ module Suj
       
       def update_root
         self.root.update_attributes(:replied_at => Time.now)
+      end
+
+      def notify_commentable_before_create
+        commentable.send(:before_create_comment, self) if commentable.respond_to?(:before_create_comment)
+      end
+      def notify_commentable_after_create
+        commentable.send(:after_create_comment, self) if commentable.respond_to?(:after_create_comment)
+      end
+
+      def notify_commentable_before_destroy
+        commentable.send(:before_destroy_comment, self) if commentable.respond_to?(:before_destroy_comment)
+      end
+
+      def notify_commentable_after_like(user)
+        commentable.send(:after_like_comment, self, user) if commentable.respond_to?(:after_like_comment)
+      end
+
+      def notify_commentable_after_unlike(user)
+        commentable.send(:after_unlike_comment, self, user) if commentable.respond_to?(:after_unlike_comment)
       end
     end
   end
--- test/dummy/app/models/book.rb
+++ test/dummy/app/models/book.rb
@@ -2,4 +2,20 @@ class Book
   include Mongoid::Document
   include Suj::Commentable
   acts_as_commentable :order => :desc, :max_depth => 3
+  
+  def before_destroy_comment(comment)
+    raise "DELETING COMMENT #{comment.text}"
+  end
+  
+  def after_create_comment(comment)
+    logger.info("CREATED COMMENTO #{comment.text}")
+  end
+  
+  def after_like_comment(comment, user)
+    logger.warn("############ User #{user.name} liked comment #{comment.text}")
+  end
+
+  def after_unlike_comment(comment, user)
+    logger.warn("############ User #{user.name} did not like comment #{comment.text}")
+  end
 end
